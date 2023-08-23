0b08c2c134b5784a4be9e83745261b49fec52c22
Aiken Drum <eikendram@gmail.com>
2023-08-23 15:57:39 +0300

    update publish script

--- publish.sh
+++ publish.sh
@@ -10,20 +10,23 @@ GOOS=linux GOARCH=amd64 go build build
 GOOS=windows GOARCH=amd64 go build build
 echo "Making archives..."
 
-tar -czvf ./publish/build-linux-amd64-$1.tar.gz ./build
-tar -czvf ./publish/build-windows-x64-$1.tar.gz ./build.exe
+tar -czvf ./publish/build-tool-linux-amd64-$1.tar.gz ./build
+tar -czvf ./publish/build-tool-windows-x64-$1.tar.gz ./build.exe
 
 echo "Making project directory..."
 cd publish
 curl -L https://github.com/EikenDram/kube-build/archive/refs/heads/main.zip -o main.zip
-unzip main.zip
+unzip -q main.zip
 rm main.zip
 mv kube-build-main kube-build
 rm -r kube-build/.vscode
 rm -r kube-build/docker
 rm -r kube-build/src
-rm kube-build/*
+mv kube-build/*.md ./
+rm kube-build/* 2> /dev/null
+rm kube-build/.gitignore
+mv *.md kube-build/
 tar cfz build-$1.tar.gz kube-build
 rm -r kube-build
 
-
+echo "Done"
