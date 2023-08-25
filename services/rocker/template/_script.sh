2709a1fe211a3fa55dd5cdeab510cf3c6f882ff8
Aiken Drum <eikendram@gmail.com>
2023-08-25 14:10:50 +0300

    added rstudio server

--- services/rocker/template/_script.sh
+++ services/rocker/template/_script.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 {{- template "script" (dict "Values" .Values "Version" .Version.rocker "Images" .Images.rocker "Value" .Values.rocker)}}
+
+{{- define "install-post"}}
+    echo "Use username 'rstudio' and password '{{.Values.cluster.password}}' to access RStudio"
+{{- end}}
